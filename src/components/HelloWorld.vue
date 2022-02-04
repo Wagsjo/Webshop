bd77a95ff7810043f94c4504f244781e42041203
Gibbem <ozmehak.calle@gmail.com>
2022-02-04 11:51:03 +0100

    added folders for components

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,3 +1,5 @@
+
+
 <script>
   export default {
     props: {
@@ -16,6 +18,11 @@
 </style>
 
 <template>
+  <h1>Raining Cats and Dogs & Bird</h1>
+  <button class='btn btn-primary' >exempel1</button>
+  <button class='btn btn-secondary' >exempel2</button>
+  <button class='btn btn-danger' >exempel3</button>
+  <button class='btn btn-info' >exempel4</button>
   <img alt="" src="/assets/fox.jpeg" />
   <p>{{ msg }}</p>
 </template>
