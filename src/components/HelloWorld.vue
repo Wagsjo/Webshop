2ae7b4f961dcff6813852f82471c0f6f9565cdf9
Gibbem <ozmehak.calle@gmail.com>
2022-02-04 12:42:14 +0100

    named project

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -17,10 +17,10 @@
 
 <template>
   <h1>Raining Cats and Dogs & Bird</h1>
-  <button class='btn btn-primary'>exempel1</button>
-  <button class='btn btn-secondary'>exempel2</button>
-  <button class='btn btn-danger'>exempel3</button>
-  <button class='btn btn-info'>exempel4</button>
-  <img alt='' src='/assets/fox.jpeg' />
+  <button class="btn btn-primary">exempel1</button>
+  <button class="btn btn-secondary">exempel2</button>
+  <button class="btn btn-danger">exempel3</button>
+  <button class="btn btn-info">exempel4</button>
+  <img alt="" src="/assets/fox.jpeg" />
   <p>{{ msg }}</p>
 </template>
