ce29e7c6393d16fc3bc1f3721b2ad4eb3304b5eb
Johannes Wågsjö <jwagsjo@hotmail.com>
2022-02-07 13:30:48 +0100

    Redigera navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,26 +1,41 @@
 <template>
-  <h1>Raining Cats and Dogs & Bird</h1>
-  <div class="pos-f-t">
-    <div class="collapse" id="navbarToggleExternalContent">
-      <div class="bg-dark p-4">
-        <h5 class="text-white h4">Collapsed content</h5>
-        <span class="text-muted">Toggleable via the navbar brand.</span>
-      </div>
-    </div>
-    <nav class="navbar navbar-dark bg-dark" style="background-color: #babfda">
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarToggleExternalContent"
-        aria-controls="navbarToggleExternalContent"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span class="navbar-toggler-icon" />
-      </button>
-    </nav>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light bg-warning text-dark">
+  <a class="navbar-brand" href="#">Logo</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <!-- DROPDOWN -->
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Djur
+        </a>
+
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Hund</a>
+          <a class="dropdown-item" href="#">Katt</a>
+          <a class="dropdown-item" href="#">Fågel</a>
+        </div>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#">Disabled</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0 ms-auto">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0 search-button" type="submit">Search</button>
+    </form>
   </div>
+</nav>
 </template>
 <script>
   export default {
@@ -28,4 +43,11 @@
   }
 </script>
 
-<style scoped></style>
+<style scoped>
+  form {
+    display: flex;
+  }
+  .search-button {
+    margin-left: 10px;
+  }
+</style>
