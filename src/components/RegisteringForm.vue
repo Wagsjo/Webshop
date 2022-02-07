df0ba0dba4757738b5d705c8620396291a59c8dd
hussein311 <Husseingh490@gmail.com>
2022-02-07 12:24:38 +0100

    add

---
+++ src/components/RegisteringForm.vue
@@ -0,0 +1,42 @@
+<script></script>
+
+<template>
+  <form id="RegForm">
+    <h1>Skapa ett konto</h1>
+    <div>
+      <label for="FirstName">förnamn</label>
+      <input type="text" class="form-control" />
+    </div>
+
+    <div class="form-group">
+      <label for="LastName">Efternamn</label>
+      <input type="text" class="form-control" />
+    </div>
+
+    <div class="form-group">
+      <label for="Email">Epost</label>
+      <input type="text" class="form-control" />
+    </div>
+
+    <div class="form-group">
+      <label for="Password">Lösenord</label>
+      <input type="text" class="form-control" />
+    </div>
+
+    <button type="submit" class="btn btn-primary">Skapa profil</button>
+  </form>
+</template>
+
+<style>
+  #RegForm {
+    max-width: 300px;
+    margin-left: auto;
+    margin-right: auto;
+  }
+  .btn-primary {
+    color: black;
+    background-color: #babfda;
+  }
+  @media (max-width: 700px) {
+  }
+</style>
---
+++ src/components/loginForm.vue
@@ -0,0 +1,33 @@
+<script></script>
+
+<template>
+  <form id="loginForm">
+    <h1>Logga in på ditt konto</h1>
+
+    <div class="form-group">
+      <label for="Email">Epost</label>
+      <input type="text" class="form-control" />
+    </div>
+
+    <div class="form-group">
+      <label for="Password">Lösenord</label>
+      <input type="text" class="form-control" />
+    </div>
+
+    <button type="submit" class="btn btn-primary">Skapa profil</button>
+  </form>
+</template>
+
+<style>
+  #loginForm {
+    max-width: 300px;
+    margin-left: auto;
+    margin-right: auto;
+  }
+  .btn-primary {
+    color: black;
+    background-color: #babfda;
+  }
+  @media (max-width: 700px) {
+  }
+</style>
