a92c0a5d608c3e751116016f8a8a5ede5bb8dca5
Gibbem <ozmehak.calle@gmail.com>
2022-02-08 12:44:03 +0100

    added firebase

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,43 @@
 <script>
   import NavBar from "./components/NavBar.vue"
-  import CounterButton from "./components/CounterButton.vue"
   import HelloWorld from "./components/HelloWorld.vue"
   import Footer from "./components/PageFooter.vue"
   import DogsViewVue from "./views/DogsView.vue"
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "firebase/app"
+  import { getFirestore, collection, getDocs } from "firebase/firestore/lite"
+
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyAy5TPe01KlpysEn4azDU8gMPGmOfUqpBY",
+    authDomain: "cats-dogs-bird.firebaseapp.com",
+    projectId: "cats-dogs-bird",
+    storageBucket: "cats-dogs-bird.appspot.com",
+    messagingSenderId: "267886520586",
+    appId: "1:267886520586:web:3a2adeefd1ee70fbd672f3",
+    measurementId: "G-TB3D0C89Y1"
+  }
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig)
+  const db = getFirestore(app)
+
+  async function getUsers(db) {
+    let users = collection(db, "users")
+    let userSnapshot = await getDocs(users)
+    let userList = userSnapshot.docs.map((doc) => doc.data())
+    console.log(userList)
+    return userList
+  }
+
+  getUsers(db)
 
   export default {
     components: {
-      CounterButton,
       HelloWorld,
       NavBar,
       DogsViewVue,
