fd7a3736d3fc5f85b4eadebf0833fd1de80a4c10
needm0resleep <kristinedelacruz.uson@gmail.com>
2022-02-08 15:40:41 +0100

    centrerade texten på djur sidorna

--- src/views/CatsView.vue
+++ src/views/CatsView.vue
@@ -25,8 +25,8 @@
       class="list-unstyled card-columns"
     >
       <li
-        class="text-uppercase display-4 fs-2 mb-4"
-        v-for="(value, name) in json.animals.cats.info"
+        class="text-uppercase display-4 fs-2 mb-4 text-center"
+        v-for="value in json.animals.cats.info"
         :key="value.name"
       >
         {{ value.name }}
