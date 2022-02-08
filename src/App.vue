b16a05a2354a20dbf09d0ea3d4e17e8c9fcb8500
Johannes Wågsjö <jwagsjo@hotmail.com>
2022-02-08 16:43:11 +0100

    Ändrat navbar

--- src/App.vue
+++ src/App.vue
@@ -51,22 +51,10 @@
 </script>
 
 <template>
+  <DogsViewVue />
+
   <nav>
     <NavBar />
-    <ul>
-      <li>
-        <RouterLink to="/">Hem</RouterLink>
-      </li>
-      <li>
-        <RouterLink to="/about">Om</RouterLink>
-      </li>
-      <li>
-        <RouterLink to="/contact">Kontakt</RouterLink>
-      </li>
-      <li>
-        <RouterLink to="/login">logga in</RouterLink>
-      </li>
-    </ul>
   </nav>
 
   <main>
